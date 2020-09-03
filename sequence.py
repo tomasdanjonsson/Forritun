3dd2acb4019d7fec5ece3a25be01365629ea936f
Tómas Dan <tomas17@ru.is>
2020-09-03 14:22:06 +0000

    Implemenation on algorithm

--- sequence.py
+++ sequence.py
@@ -1,2 +1,13 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
+for i in range(1, n + 1):
+    if i == 1:
+        sequence = one = i
+    elif i == 2:
+        sequence = two = i
+    elif i == 3:
+        sequence = three = i
+    else:
+        sequence = one + two + three
+        one, two, three = two, three, sequence
+print(sequence)
